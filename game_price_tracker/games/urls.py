149ca91f557c0de793fa7d208eac31dc04ff7a8e
remotionis <78084978+remotionis@users.noreply.github.com>
2025-04-24 09:07:02 +0900

    Update urlpatterns

--- game_price_tracker/games/urls.py
+++ game_price_tracker/games/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('', get_game_list, name='game-list-create'),
     path('games/', GameListCreateView.as_view(), name='game-list'),
-    path('<int:pk>/', GameRetrieveUpdateDestroyView.as_view(), name='game-detail'),
-    path('<int:pk>/price-history', PriceHistoryView.as_view(), name='price-history'),
+    path('games/<int:pk>/', GameRetrieveUpdateDestroyView.as_view(), name='game-detail'),
+    path('games/<int:pk>/price-history', PriceHistoryView.as_view(), name='price-history'),
     path('genres/', GenreList.as_view(), name='genre-list'),
     path('languages/', LanguageList.as_view(), name='language-list'),
 ]
